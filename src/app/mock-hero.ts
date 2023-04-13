02e985b166797d49dd0d8c8b34b35a320f779f8b
alex barroso <alexjunior651@hotmail.com>
2023-04-13 02:35:51 -0300

    Creating the hero-detail component

---
+++ src/app/mock-hero.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Homem Aranha'},
+  {id: 3, name: 'Batman'},
+  {id: 4, name: 'Saitama'},
+  {id: 5, name: 'Rimuru'},
+  {id: 6, name: 'Luffy'},
+  {id: 7, name: 'Roronoa Zoro'},
+  {id: 8, name: 'Sanjy'},
+  {id: 9, name: 'Capitão América'},
+  {id: 10, name: 'Hulk'},
+];
