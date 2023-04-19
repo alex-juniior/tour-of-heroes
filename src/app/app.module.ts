f77966a55283bab997a7900b87d2ed216abc86e9
alex barroso <alexjunior651@hotmail.com>
2023-04-19 01:26:44 -0300

    Creation of the messages component, addition of the clear messages button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,25 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MessagesComponent } from './messages/messages.component';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,37 @@
+import { HeroService } from '../services/hero.service';
+import { Hero } from './../hero.model';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero.model';
-import { HEROES } from '../mock-hero';
+import { Observable } from 'rxjs';
+import { MessageService } from '../services/message.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent{
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void{
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+     this.heroService.getHeroes().subscribe((
+      heroes) =>(this.heroes = heroes)
+      );
+  }
+
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Select hero id =${hero.id}`)
   }
 
 }
