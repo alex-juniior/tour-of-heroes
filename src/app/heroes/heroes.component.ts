57e7744f1673c1023376b274db67375e10b6f493
alex barroso <alexjunior651@hotmail.com>
2023-04-21 02:20:26 -0300

    Route adjustment

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { HeroService } from '../services/hero.service';
 import { Hero } from './../hero.model';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { MessageService } from '../services/message.service';
 
 
@@ -12,10 +11,8 @@ import { MessageService } from '../services/message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService,
-    private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
 
   }
 
@@ -27,11 +24,4 @@ export class HeroesComponent implements OnInit {
       heroes) =>(this.heroes = heroes)
       );
   }
-
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Select hero id =${hero.id}`)
-  }
-
 }
